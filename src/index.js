ed5609db616f37d20bf680ca6a1c79493ecbf053
Justin Abraham <abrahamjustin24@gmail.com>
2024-10-18 18:54:00 -0400

    Implemented API endpoints

--- src/index.js
+++ src/index.js
@@ -9,6 +9,46 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
+var users = []
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const randid = Math.floor(Math.random() * 1000);
+    users.push({name, email, id: randid})
+    res.status(201).json({ name: name, email: email, id: randid});
+})
+
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+    const foundUser = users.find(x => x.id == id);
+    if (!foundUser) {
+        res.status(404).send();
+    }
+    res.status(200).json({ id: Number(id), name: foundUser.name, email: foundUser.email })
+})
+
+app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const foundUserIndex = users.findIndex(x => x.id == id);
+    if (!foundUserIndex) {
+        res.status(404).send();
+    }
+    users[foundUserIndex].name = name;
+    users[foundUserIndex].email = email;
+    res.status(200).json({ id: id, name: name, email: email });
+})
+
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    try {
+        users = users.filter(x => x.id != id);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+    res.status(204).send();
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
